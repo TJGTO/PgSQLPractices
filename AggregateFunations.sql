4835605d6c39a7cdd98f9047463932a303278696
Tathagata Mondal <tathagata.mandal@zimyo.com>
2021-11-07 00:51:00 +0530

    Grouping

--- AggregateFunations.sql
+++ AggregateFunations.sql
@@ -20,3 +20,18 @@ SELECT MIN(movie_length) FROM movies;
                        /*AVG*/
 
 SELECT AVG(movie_length) FROM movies;                   
+
+                      /* GROUPING */
+
+SELECT movie_lang, COUNT(movie_lang) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang, AVG(movie_length) FROM movies
+GROUP BY movie_lang;
+
+SELECT movie_lang,age_certificate AVG(movie_length) FROM movies
+GROUP BY movie_lang,age_certificate;
+
+SELECT movie_lang , MIN(movie_length) , MAX(movie_length) FROM movies
+WHERE age_certificate = '15'
+GROUP BY movie_lang;
