b22e41bf05da657ba813f7855af6da8be771e74c
Tathagata Mondal <tathagata.mandal@zimyo.com>
2021-11-14 23:31:26 +0530

    Changes

--- DML.sql
+++ DML.sql
@@ -24,3 +24,40 @@ DELETE FROM examples
 WHERE nationality = 'GBR';
 
 DELETE FROM examples; --//delete all the rows from the table
+
+-- 1. Insert the data into the owners table
+
+INSERT INTO owners (first_name, last_name, city, state, email)
+VALUES ('Samuel','Smith','Boston','MA','samsmith@gmail.com'),
+('Emma','Johnson','Seattle','WA','emjohnson@gmail.com'),
+('John','Oliver','New York','NY','johnoliver@gmail.com'),
+('Olivia','Brown','San Francisco','CA','oliviabrown@gmail.com'),
+('Simon','Smith','Dallas','TX','sismith@gmail.com'),
+(null,'Maxwell',null,'CA','lordmaxwell@gmail.com');
+
+SELECT * FROM owners;
+
+-- 2. Insert the data into the pets table
+
+INSERT INTO pets (species, full_name, age, owner_id)
+VALUES ('Dog','Rex',6,1),
+('Rabbit','Fluffy',2,5),('Cat','Tom',8,2),('Mouse','Jerry',2,2),
+('Dog','Biggles',4,2),('Tortoise','Squirtle',42,3);
+
+SELECT * FROM pets;
+
+-- 3. Update Fluffy the rabbits age to 3
+
+UPDATE pets
+SET age = 3
+WHERE full_name = 'Fluffy';
+
+SELECT * FROM pets;
+
+-- 4. Delete Mr Maxwell from the owners table
+
+SELECT * FROM owners;
+
+DELETE FROM owners
+WHERE last_name = 'Maxwell';
+
--- RetrivingDateFromTables.sql
+++ RetrivingDateFromTables.sql
@@ -23,6 +23,29 @@ WHERE release_date > '1999-12-31'; // Logical Operators on Date
 SELECT * FROM movies
 WHERE movie_lang > 'English'; // will search alphabatically after 'E' 
 
+-- 1. Select the movie_name and release_date of every movie. 
+
+SELECT movie_name, release_date FROM movies;
+
+-- 2. Select the first and last names of all American directors. 
+
+SELECT first_name, last_name FROM directors 
+WHERE nationality = 'American';
+
+-- 3. Select all male actors born after the 1st of January 1970. 
+
+SELECT * FROM actors
+WHERE gender = 'M'
+AND date_of_birth > '1970-01-01';
+
+-- 4. Select the names of all movies which are over 90 minutes long and movie
+-- language is English.
+
+SELECT movie_name FROM movies
+WHERE movie_length > 90 
+AND movie_lang = 'English';
+
+
 /*// IN and not in //////////////////////////////////////////*/
 SELECT first_name, last_name FROM actors
 WHERE  first_name IN ('Bruce','John','Peter'); //select on multiple values
@@ -43,6 +66,27 @@ WHERE release_date BETWEEN '1995-01-01' AND '1999-12-31';
 
 SELECT movie_name , movie_length FROM movies
 WHERE movie_length BETWEEN 90 AND 120;
+
+-- 1.Select the movie names and movie language of all movies with a movie language of English, 
+-- Spanish or Korean. 
+
+SELECT movie_name, movie_lang FROM movies
+WHERE movie_lang IN ('English','Spanish','Korean');
+
+-- Select the first and last names of the actors whose last name begins with M and were born 
+-- between 01/01/1940 and 31/12/1969. 
+
+SELECT first_name, last_name FROM actors
+WHERE last_name LIKE 'M%'
+AND date_of_birth BETWEEN '1940-01-01' AND '1969-12-31';
+
+-- Select the first and last names of the directors with nationality of British, French or 
+-- German born between 01/01/1950 and 31/12/1980. 
+
+SELECT first_name, last_name FROM directors
+WHERE nationality IN ('British','French','German')
+AND date_of_birth BETWEEN '1950-01-01' AND '1980-12-31';
+
 
 /*/Order BY //////////////////////////////////////*/
 SELECT first_name,last_name,date_of_birth FROM actors
@@ -86,6 +130,24 @@ SELECT DISTINCT movie_lang, age_certificate FROM movies
 ORDER BY movie_lang;
 /*return the distinct combinations of 2 columns*/
 
+-- 1. Select the American directors ordered from oldest to youngest. 
+
+SELECT * FROM directors
+WHERE nationality = 'American'
+ORDER BY date_of_birth;
+
+-- 2. Return the distinct nationalities from the directors table. 
+
+SELECT DISTINCT nationality FROM directors;
+
+-- 3. Return the first names, last names and date of births of the 10 youngest female actors.
+
+SELECT first_name, last_name, date_of_birth FROM actors
+WHERE gender = 'F'
+ORDER BY date_of_birth DESC
+LIMIT 10;
+
+
 /*/NULL //////////////////////////////////////*/
 
 SELECT * FROM actors
@@ -110,3 +172,22 @@ SELECT CONCAT(first_name , last_name) AS full_name FROM actors;
 SELECT CONCAT(first_name ,'-', last_name) AS full_name FROM actors;
 SELECT CONCAT_WS(' ',first_name,middle_name, last_name) AS full_name FROM actors;
 /*Ws means with separetor, it will result 'first_name middle_name last_name'*/
+
+-- 1. Return the top 3 movies with the highest international takings. 
+
+SELECT * FROM movie_revenues
+WHERE international_takings IS NOT NULL
+ORDER BY international_takings DESC
+LIMIT 3;
+
+-- 2. Concatenate the first and last names of the directors, separated by a space, 
+-- and call this new column full_name. 
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM directors;
+
+-- 3. Return the actors with missing first_names or missing date_of_births. 
+
+SELECT * FROM actors
+WHERE first_name IS NULL 
+OR date_of_birth IS NULL;
+
