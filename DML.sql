037060084d822d777a80a8cfbc9235868ed6a832
Tathagata Mondal <tathagata.mandal@zimyo.com>
2021-11-14 23:18:15 +0530

    update

--- DML.sql
+++ DML.sql
@@ -1,11 +1,11 @@
-//create statement/////////////////////////////////////
+--//create statement/////////////////////////////////////
 INSERT INTO examples (first_name , last_name , email , nationality, age)
 VALUES ('Value1','Value1','Value1@gmail.com','Value1',55),
 ('Value2','Value2','Value2@gmail.com','Value2',54),
 ('Value3','Value3','Value3@gmail.com','Value3',56)
 ;
 
-//update statement/////////////////////////////////////
+--//update statement/////////////////////////////////////
 UPDATE examples 
 SET email = 'davidmitchel@gmail.com'
 WHERE example_id = 1;
@@ -15,12 +15,12 @@ SET nationality = 'CAN'
 WHERE natonality = 'USA';
 
 UPDATE examples
-SET first_name = 'JAMES' , age = 55 //multiple colums update
+SET first_name = 'JAMES' , age = 55 --//multiple colums update
 WHERE example_id = 5;
 
-//Delete statement/////////////////////////////////////
+--//Delete statement/////////////////////////////////////
 
 DELETE FROM examples
 WHERE nationality = 'GBR';
 
-DELETE FROM examples; //delete all the rows from the table
+DELETE FROM examples; --//delete all the rows from the table
